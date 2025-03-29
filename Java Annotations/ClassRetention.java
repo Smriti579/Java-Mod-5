bb494e8a2ee19c64036c6ae1dced6cee6d0b8ede
Smriti579 <smritimahajan579@gmail.com>
2025-03-29 22:45:07 +0530

    Delete Java Annotations/ClassRetention.java

--- Java Annotations/ClassRetention.java
+++
@@ -1,9 +0,0 @@
-import java.lang.annotation.*;
-
-@Retention(RetentionPolicy.CLASS)
-@interface CompileTimeAnnotation {}
-
-public class Example {
-    @CompileTimeAnnotation
-    public void display() {}
-}
